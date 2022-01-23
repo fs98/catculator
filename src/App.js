d90b42b6d258bdb999731070803101e92b746631
Fata Sefer <sefer.fata@gmail.com>
2022-01-24 00:19:23 +0100

    stretch the structure with flex

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import Calculator from "./components/Calculator/Calculator";
+
 function App() {
   return (
-    <div>Catculator</div>
+    <div className="flex flex-col h-screen justify-between">
+      <header>Catculator Header</header>
+      <div className="flex-grow">
+        <Calculator />
+      </div>
+      <footer>&copy; 2022 Catculator</footer>
+    </div>
   );
 }
 
