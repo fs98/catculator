a35e26c10c83f90867a1d81b0b36ce065a747225
Fata Sefer <sefer.fata@gmail.com>
2022-01-24 01:12:53 +0100

    add onClick to the button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,15 +1,15 @@
-const Button = ({ type, children }) => {
+const Button = ({ type, onClick, children }) => {
   const className =
     type === "action"
       ? "bg-orange-500 hover:bg-orange-600"
       : "bg-gray-300 hover:bg-gray-400";
 
   return (
-    <div
+    <button onClick={onClick}
       className={`px-7 py-8 text-4xl text-center hover:cursor-pointer hover:trasform hover:-translate-y-2 active:translate-y-0 ${className}`}
     >
       {children}
-    </div>
+    </button>
   );
 };
 
