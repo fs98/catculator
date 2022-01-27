6892aa717365bbb8471098ef5b5c35724cee6b75
Fata Sefer <sefer.fata@gmail.com>
2022-01-27 22:34:28 +0100

    add header component, link to GitHub

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Calculator from "./components/Calculator/Calculator";
+import Header from "./components/Header/Header";
 
 function App() {
   
   return (
     <div className="flex flex-col h-screen justify-between bg-black-cat bg-contain bg-no-repeat bg-right-top">
-      <header>Catculator Header</header>
+      <Header />
       <div className="flex-grow">
         <Calculator />
       </div>
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,28 @@
+import { TiSocialGithub } from "react-icons/ti";
+
+const Header = () => (
+  <header>
+    <nav className="h-12 container mx-auto flex justify-between">
+      <a
+        href="/"
+        className="py-3 flex items-center content-start text-gray-500 hover:opacity-75"
+      >
+        &nbsp; Catculator by Fata
+      </a>
+
+      <ul className="flex items-center space-x-2">
+        <li>
+          <a
+            href="https://github.com/fs98/catculator"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <TiSocialGithub color="#333" size="32" />
+          </a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+);
+
+export default Header;
