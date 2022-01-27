a81f253863bd4b5b3378f1bff2f1f07c07694994
Fata Sefer <sefer.fata@gmail.com>
2022-01-27 22:28:17 +0100

    add custom background

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import Calculator from "./components/Calculator/Calculator";
 
 function App() {
+  
   return (
-    <div className="flex flex-col h-screen justify-between">
+    <div className="flex flex-col h-screen justify-between bg-black-cat bg-contain bg-no-repeat bg-right-top">
       <header>Catculator Header</header>
       <div className="flex-grow">
         <Calculator />
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'black-cat': "url('/src/img/cat.png')"
+      }
+    },
   },
   plugins: [],
 }
